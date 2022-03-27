669ef3d7b7a0ebd615ca41534fede9b73a4eea1f
faisalahmadian24 <101980554+faisalahmadian24@users.noreply.github.com>
2022-03-27 18:55:50 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Faisal Ahmadian
+        Saya tinggal di perum. panggon mas jl.cempaka no 3
+        Olah raga yang saya sukai adalah 
+        Bulutangkis";
+    }
+}
+?>
